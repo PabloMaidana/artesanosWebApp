88a44ce88151c7001406256d57990bc9ebec9e07
PabloMaidana <pablomaid1@gmail.com>
2025-06-17 20:29:07 -0300

    packagejason

--- app.js
+++ app.js
@@ -91,5 +91,5 @@ io.on('connection', (socket) => {
 // iniciar servidor
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${PORT}`);
 });
